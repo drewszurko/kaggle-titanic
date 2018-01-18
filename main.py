16faef6d2f6c87cfc8d62b038274b15035f9f023
Drew Szurko <andrewszurko@gmail.com>
2018-01-17 23:02:02 -0600

    Removed scaler since features are now grouped into int buckets.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 from models import fit_models, gridsearch_cv
-from sklearn.preprocessing import StandardScaler
 import data_utils
 
 if __name__ == '__main__':
@@ -14,18 +13,13 @@ if __name__ == '__main__':
     # Load predict feature values.
     X_predict = df_predict.iloc[:, 0:].values
 
-    # Scale our data.
-    scaler = StandardScaler()
-    scaler = scaler.fit(X)
-    X_scaled = scaler.transform(X)
-    X_pred_scaled = scaler.transform(X_predict)
-
+    # Get x shape for our NN input.
     x_shape = X.shape[1]
 
     # Gridsearch models.
     k1_params, k2_params, xg1_params, rfc1_params, etc1_params, abc1_params, gbc1_params, svc1_params = gridsearch_cv(
-        X_scaled, y, x_shape)
+        X, y, x_shape)
 
     # Predict on models.
     fit_models(k1_params, k2_params, xg1_params, rfc1_params, etc1_params, abc1_params, gbc1_params, svc1_params,
-               X_scaled, y, X_pred_scaled, df_predict)
+               X, y, X_predict, df_predict)
