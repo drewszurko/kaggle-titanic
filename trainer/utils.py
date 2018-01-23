c4514b25b37f998657ee34185abe26a2b24c5771
Drew Szurko <andrewszurko@gmail.com>
2018-01-22 22:51:08 -0600

    Updated to include absolute path for prediction csv dumps.

--- trainer/utils.py
+++ trainer/utils.py
@@ -1,9 +1,12 @@
-import pandas as pd
 from datetime import datetime
 from tensorflow.python.lib.io import file_io
+import pandas as pd
+import os
+
+_PROJECT_ROOT = os.pardir
 
 _CSV_CLOUD_PATH = 'gs://keras-titanic-models/data/predictions/'
-_CSV_LOCAL_PATH = 'csv/'
+_CSV_LOCAL_PATH = 'data/predictions/'
 
 
 def predictions_to_csv(predictions, passenger_ids, cloud_train):
@@ -19,7 +22,7 @@ def predictions_to_csv(predictions, passenger_ids, cloud_train):
 
     if not cloud_train:
         # Save predictions to local storage.
-        df_submit.to_csv(_CSV_LOCAL_PATH + filename, index=False)
+        df_submit.to_csv(os.path.join(_PROJECT_ROOT, _CSV_LOCAL_PATH, filename), index=False)
     else:
         # Save predictions to the cloud storage bucket's jobs directory.
         with file_io.FileIO(_CSV_CLOUD_PATH + filename, mode='w+') as input_f:
