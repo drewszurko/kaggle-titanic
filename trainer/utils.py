b2a51395e1a479d937e96142421beea3b7473ff0
Drew Szurko <andrewszurko@gmail.com>
2018-01-22 20:00:45 -0600

    Added local and cloud support when saving Kaggle predictions.

---
+++ trainer/utils.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from datetime import datetime
+from tensorflow.python.lib.io import file_io
+
+_CSV_CLOUD_PATH = 'gs://keras-titanic-models/data/predictions/'
+_CSV_LOCAL_PATH = 'csv/'
+
+
+def predictions_to_csv(predictions, passenger_ids, cloud_train):
+    # Get current time. Will be prepended to filename.
+    date_str = datetime.now().strftime('%Y%m%d_%H%M%S')
+
+    # Generate csv filename.
+    filename = date_str + '_titanic_predictions'
+
+    # Generate kaggle submission file.
+    df_submit = pd.DataFrame({'PassengerId': passenger_ids,
+                              'Survived': predictions})
+
+    if not cloud_train:
+        # Save predictions to local storage.
+        df_submit.to_csv(_CSV_LOCAL_PATH + filename, index=False)
+    else:
+        # Save predictions to the cloud storage bucket's jobs directory.
+        with file_io.FileIO(_CSV_CLOUD_PATH + filename, mode='w+') as input_f:
+            df_submit.to_csv(input_f, index=False)
